596996ff66f184c97be36d46d42ae1b053bde943
Tom Molinaro <tommolinaro@Toms-Mac-mini.local>
2025-09-23 06:33:00 -0400

    new project jurassic park

---
+++ ApexPredators/ApexPredators/ApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApexPredatorsApp.swift
+//  ApexPredators
+//
+//  Created by Tom Molinaro on 9/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
