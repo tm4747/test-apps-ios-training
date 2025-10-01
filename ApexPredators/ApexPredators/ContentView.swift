9264ac2fb08518c8b9a29cb69abd2fa460686d43
Tom Molinaro <tommolinaro@Toms-Mac-mini>
2025-10-01 05:57:54 -0400

    restore point before moving search functionality into predators

--- ApexPredators/ApexPredators/ContentView.swift
+++ ApexPredators/ApexPredators/ContentView.swift
@@ -9,49 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     let predators = Predators()
+    
+    @State var searchText = ""
+    
+    var filteredDinos: [ApexPredator] {
+        if searchText.isEmpty {
+            return predators.apexPredators
+        } else {
+            return predators.apexPredators.filter {
+                predator in predator.name.localizedCaseInsensitiveContains(searchText)
+            }
+        }
+    }
+    
     var body: some View {
-        List(predators.apexPredators){ predator in
-            HStack {
-                Image(predator.image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width:100, height: 100)
-                    .shadow(color: .white, radius: 1)
-                VStack(alignment: .leading) {
-                    // name
-                    Text(predator.name)
-                        .fontWeight(.bold)
-                    Text(predator.type.rawValue.capitalized)
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .padding(.horizontal, 13)
-                        .padding(.vertical, 5)
-                        .background(predator.type.background)
-                        .clipShape(.capsule)
+        NavigationStack {
+            List(filteredDinos){ predator in
+                NavigationLink {
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                } label: {
+                    HStack {
+                        Image(predator.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width:100, height: 100)
+                            .shadow(color: .white, radius: 1)
+                        VStack(alignment: .leading) {
+                            // name
+                            Text(predator.name)
+                                .fontWeight(.bold)
+                            Text(predator.type.rawValue.capitalized)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.horizontal, 13)
+                                .padding(.vertical, 5)
+                                .background(predator.type.background)
+                                .clipShape(.capsule)
 
+                        }
+                    }
                 }
             }
-//            HStack {
-//                // Dinosaur image
-//                Image(predator.image)
-//                    .resizable()
-//                    .scaledToFit()
-//                    .frame(width:100, height: 100)
-//                    .shadow(color: .white, radius: 1)
-//                
-//                VStack(alignment: .leading) {
-//                    // name
-//                    Text(predator.name)
-//                        .fontWeight(.bold)
-//                    // type
-//                    Text(predator.type.capitalized)
-//                        .font(.subheadline)
-//                        .fontWeight(.semibold)
-//                        .padding(.horizontal, 13)
-//                        .padding(.vertical, 5)
-//                    
-//                }
-//            }
+            .navigationTitle("Apex Predators")
+            .searchable(text: $searchText)
+            .autocorrectionDisabled()
+            .animation(.default, value: searchText)
         }
         .preferredColorScheme(.dark)
     }
