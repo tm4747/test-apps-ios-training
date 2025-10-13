43b9142185c4e8d24fd28d22297918d96ba9bd34
tm4747 <tm4747@hotmail.com>
2025-10-13 06:46:18 -0400

    updates

--- BBQuotes/BBQuotes/QuoteView.swift
+++ BBQuotes/BBQuotes/QuoteView.swift
@@ -22,8 +22,35 @@ struct QuoteView: View {
                     .frame(width: geo.size.width * 2.7, height: geo.size.height * 1.2)
                     
                 VStack {
-                    Text("\"\"")
+                    Text("\"\(vm.quote.quote)\"")
+                        .multilineTextAlignment(.center)
+                        .foregroundStyle(.white)
+                        .padding()
+                        .background(.black.opacity(0.5))
+                        .clipShape(.rect(cornerRadius:25))
+                        .padding(.horizontal)
+                    
+                    ZStack(alignment: .bottom) {
+                        AsyncImage(url: vm.character.images[0]) { image in
+                            image
+                                .resizable()
+                                .scaledToFill()
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        .frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
+                        
+                        Text(vm.quote.character)
+                            .foregroundStyle(.white)
+                            .padding(10)
+                            .frame(maxWidth: .infinity)
+                            .background(.ultraThinMaterial)
+                            .fontWeight(.bold)
+                    }
+                    .frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
+                    .clipShape(.rect(cornerRadius: 50))
                 }
+                .frame(width: geo.size.width)
             }
             .frame(width: geo.size.width, height: geo.size.height)
         }
