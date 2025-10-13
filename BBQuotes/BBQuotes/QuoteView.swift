db9c0513d0f7da62ed5c4b1468d6a9130ca482b9
Tom Molinaro <tommolinaro@Toms-Mac-mini>
2025-10-13 10:51:51 -0400

    Updates

--- BBQuotes/BBQuotes/QuoteView.swift
+++ BBQuotes/BBQuotes/QuoteView.swift
@@ -22,35 +22,73 @@ struct QuoteView: View {
                     .frame(width: geo.size.width * 2.7, height: geo.size.height * 1.2)
                     
                 VStack {
-                    Text("\"\(vm.quote.quote)\"")
-                        .multilineTextAlignment(.center)
-                        .foregroundStyle(.white)
-                        .padding()
-                        .background(.black.opacity(0.5))
-                        .clipShape(.rect(cornerRadius:25))
-                        .padding(.horizontal)
-                    
-                    ZStack(alignment: .bottom) {
-                        AsyncImage(url: vm.character.images[0]) { image in
-                            image
-                                .resizable()
-                                .scaledToFill()
-                        } placeholder: {
-                            ProgressView()
-                        }
-                        .frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
+                    // Forces the 2 spacers to take up all the space of the parent view, therefore leaving minimal space for the button, and furthermore, pushing the button to the bottom of the screen
+                    VStack{
+                        Spacer(minLength:60)
                         
-                        Text(vm.quote.character)
-                            .foregroundStyle(.white)
-                            .padding(10)
-                            .frame(maxWidth: .infinity)
-                            .background(.ultraThinMaterial)
-                            .fontWeight(.bold)
+                        switch vm.status {
+                        case .notStarted:
+                            EmptyView()
+                        case .fetching:
+                            ProgressView()
+                        case .success:
+                            Text("\"\(vm.quote.quote)\"")
+                                // will scale down as much as 50% to make room for excessive text
+                                .minimumScaleFactor(0.5)
+                                .multilineTextAlignment(.center)
+                                .foregroundStyle(.white)
+                                .padding()
+                                .background(.black.opacity(0.5))
+                                .clipShape(.rect(cornerRadius:25))
+                                .padding(.horizontal)
+                            
+                            ZStack(alignment: .bottom) {
+                                AsyncImage(url: vm.character.images[0]) { image in
+                                    image
+                                        .resizable()
+                                        .scaledToFill()
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                .frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
+                                
+                                Text(vm.quote.character)
+                                    .foregroundStyle(.white)
+                                    .padding(10)
+                                    .frame(maxWidth: .infinity)
+                                    .background(.ultraThinMaterial)
+                                    .fontWeight(.bold)
+                            }
+                            .frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
+                            .clipShape(.rect(cornerRadius: 50))
+                        case .failed(let error):
+                            Text(error.localizedDescription)
+                        }
+                                                
+                        Spacer()
                     }
-                    .frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
-                    .clipShape(.rect(cornerRadius: 50))
+                    
+                    
+                    
+                    Button {
+                        // allows a unit of asyncronous work to run in syncronous environments
+                        Task {
+                            await vm.getData(for: show)
+                        }
+                    } label: {
+                        Text("Get Random Quote")
+                            .font(.title)
+                            .foregroundStyle(.white)
+                            .padding()
+                            .background(.breakingBadGreen.opacity(0.75))
+                            .clipShape(.rect(cornerRadius:10))
+                            .shadow(color: .breakingBadYellow, radius: 5)
+                    }
+                    
+                    Spacer(minLength: 95)
+                    
                 }
-                .frame(width: geo.size.width)
+                .frame(width: geo.size.width, height: geo.size.height)
             }
             .frame(width: geo.size.width, height: geo.size.height)
         }
